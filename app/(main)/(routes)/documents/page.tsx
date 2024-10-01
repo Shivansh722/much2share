91b975661b3a8c0b2984526030a6791f77a1b687
Shivansh722 <shivanshcha722@gmail.com>
2024-10-01 13:30:13 +0530

    feat: add hero to empty document page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,39 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react"; // Assuming PlusCircle is from this package
+import Image from "next/image"; // If you're using Next.js
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+    const firstName = user?.firstName || 'Guest';
+
     return (
-        <div>
-            This is a protected DocumentsPage
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+                src="/empty_dark.png"
+                height={300}
+                width={300}
+                alt="Empty"
+                className="hidden dark:block"
+            />
+            <Image 
+                src="/empty_lig.png"
+                height={300}
+                width={300}
+                alt="Empty"
+                className="dark:hidden"
+            />
+            
+            <h2>
+                Welcome to {firstName}&apos;s Meet2share
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2" />
+                Create a note
+            </Button>
         </div>
-     );
+    );
 }
  
 export default DocumentsPage;
