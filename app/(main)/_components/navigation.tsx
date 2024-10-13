86d52878786100db96df125c734972ffad5f0387
Shivansh722 <shivanshcha722@gmail.com>
2024-10-13 10:48:53 +0530

    feat: Add user profile in sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -111,7 +112,7 @@ export const Navigation = () => {
         </div>
 
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
 
         <div className="mt-4">
---
+++ app/(main)/_components/user-item.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export const UserItem = () => {
+    return <div>UserItem</div>;
+    }
+    
